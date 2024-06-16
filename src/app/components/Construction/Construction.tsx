c44a8599c14d5f32e990cf928b7d29577e90e864
Alexandre <alexandre.caissy@norda.com>
2024-06-16 16:02:03 -0400

    feat: under construction

---
+++ src/app/components/Construction/Construction.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Construction = (props: Props) => {
+  return (
+    <div className="under-construction">
+      <h1>UNDER CONSTRUCTION</h1>
+    </div>
+  );
+};
+
+export default Construction;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Baloo_2 } from "next/font/google";
 import "@/app/assets/styles/globals.scss";
 import Navigation from "@/app/components/Navigation/Navigation";
+import Construction from "./components/Construction/Construction";
 const baloo = Baloo_2({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={baloo.className}>
         <Navigation />
-        {children}
+        <Construction />
+        {/* {children} */}
       </body>
     </html>
   );
